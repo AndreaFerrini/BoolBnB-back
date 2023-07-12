3315813e94f077a61deec3780a8d73122aa68d60
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-07-12 16:18:44 +0200

    add custom request

--- app/Http/Requests/Auth/RegisteredUserRequest.php
+++ app/Http/Requests/Auth/RegisteredUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules;
 
 class RegisteredUserRequest extends FormRequest
 {
@@ -24,7 +25,27 @@ class RegisteredUserRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'email'=>['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'name' => ['string', 'max:255', 'nullable'],
+            'surname' => ['string', 'max:255', 'nullable'],
+            'birth' => ['date', 'nullable'],
+
         ];
     }
+    public function attributes(): array
+   {
+       return [
+        'name' => 'il nome del progetto',
+        'description' => 'la descrizione del progetto',
+        'short_description' => 'la descrizione BREVE del progetto',
+        'image' => 'la foto del progetto',
+        'relase_date' => 'la data di creazione del progetto',
+        'type_id' => 'i programmi usati per il progetto',
+        'visbility' => 'la visibiltà del progetto',
+        'tags' => 'i tags',
+        'project_link' => 'il link del progetto'
+
+       ];
+   }
 }
