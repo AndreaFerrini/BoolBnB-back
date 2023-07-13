0a02a716708905eeae94f3ba7477d841ffa0468c
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-07-13 22:11:30 +0900

    nuovo commit

--- config/Data_For_Seeding/bnb_api_client_functions.php
+++ config/Data_For_Seeding/bnb_api_client_functions.php
@@ -3,16 +3,16 @@
     use GuzzleHttp\Client as Client;
 
     const base_url  =   "https://api.tomtom.com/search/2/geocode/";
-    const api_key   =   'H87cql0ALbfterWC1evVSLYCYtbFAkxd';
     $error_index    =   0;
     $error_message  =   "";
 
     function get_coordinates($address, $city)
     {
+        $api_key = env('TOMTOM_API_KEY');
         if (isset($address) && $address !== "" && isset($city) && $city !== "")
         {
             $client_call = new GuzzleHttp\Client(['verify' => false]);
-            $end_point = $address . "" . $city . ".json?key=" . api_key;
+            $end_point = $address . "" . $city . ".json?key=" . $api_key;
             $whole_url = base_url . $end_point;
             $response = $client_call->get($whole_url);
             return $response->getBody()->getContents();
