b8dc2db9ba641f35a47226d70f538cc02208ed16
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-07-13 12:42:15 +0900

    Regola inserita dentro la specifica request

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,11 +27,11 @@
                             <div class="col-md-6">
                                 <input id="birth" type="date" class="form-control @error('birth') is-invalid @enderror" name="birth" value="{{ old('birth') }}"autocomplete="birth" autofocus style="min-width: 150px">
 
-                                @error('name', 'surname', 'birth')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
+                                {{-- @error('birth')
+                                    <span class="alert alert-danger" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror --}}
                             </div>
                         </div>
 
