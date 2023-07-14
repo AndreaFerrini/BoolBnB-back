8a75f334cc36e7d035275a76979d13aed00a7170
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-07-14 14:53:56 +0200

    organizzazione view personale

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,34 +3,69 @@
 @section('content')
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
+        I tuoi Annunci
     </h2>
     <div class="row justify-content-center">
         <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
-
+            <div class="card"  id="welcome_banner">
                 <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    Sei loggato!
                 </div>
             </div>
         </div>
     </div>
     <div>
-        <a href="{{route('admin.apartments.index')}}">index</a>
         <a href="{{route('admin.apartments.create')}}">create</a>
-        @isset($apartments)
-            @foreach ($apartments as $apartment)
-                <span>{{$apartment->id}}</span>
-                <span>{{$apartment->title}}</span>
-            @endforeach
-        @endisset
+        @if(isset($apartments))
+        <div class="card p-3">
+            <div class="row">
+                @foreach ($apartments as $apartment)
+                <div class="p-2 col-4">
+                    <div class="card">
+                        <h5 class="card-title text-center my-2">{{$apartment->title}}</h5>
+                        <img class="card-img-top" src="https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg" alt="Card image cap">
+                        <div class="card-body">
+
+                            <p class="card-text">{{$apartment->description}}</p>
+                            <p><i>{{$apartment->address}}, {{$apartment->city}}</i></p>
+                            <span><b>Longitudine:</b> {{$apartment->longitude}}</span>
+                            <span><b>Latitudine:</b> {{$apartment->latitude}}</span>
+                            <div>
+                                <span><b>Stanze:</b>{{$apartment->number_of_rooms}}</span>
+                                <span><b>Bagni:</b>{{$apartment->number_of_bathrooms}}</span>
+                                <span><b>Superficie:</b>{{$apartment->square_meters}}Mq</span>
+                            </div>
+                            <div>
+                                @foreach ($apartment->services as $service)
+                                <span>
+                                    {{$service->name}}
+                                    <i class="{{$service->icon}}"></i>
+                                </span>
+                                @endforeach
+                            </div>
+
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+
+        @endif
     </div>
 </div>
+
+<script>
+    setTimeout(function () {
+        document.getElementById('welcome_banner').classList.add('d-none');
+    }, 2000);
+</script>
 @endsection
+
+
