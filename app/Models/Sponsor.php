9bf186890c0c8f381a16395123d1e9dce8156e4b
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-07-14 12:29:01 +0900

    Model Sponsor + metodo relazionale apartments: OK

--- app/Models/Sponsor.php
+++ app/Models/Sponsor.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Sponsor extends Model
 {
     use HasFactory;
+
+    protected   $table      = "Sponsors";
+
+    protected   $fillable   = ['name', 'period', 'price']; 
+
+    public  function apartments() 
+    {
+        return $this->belongsToMany(Apartment::class);
+    }
 }
