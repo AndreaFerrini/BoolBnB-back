bde525cb491c622ce8f1ff55cfd6dcc69563b1c2
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-07-15 18:08:27 +0900

    Aggiunta cap

--- config/Data_For_Seeding/bnb_database_for_seeding.php
+++ config/Data_For_Seeding/bnb_database_for_seeding.php
@@ -2,263 +2,315 @@
 
     return [
         [
-            'city' => 'Firenze',
-            'address' => 'Piazza dei Giuochi, 1',
-            'name' => 'Dimora Le Torri'
+            'city'      => 'Firenze',
+            'address'   => 'Piazza dei Giuochi, 1',
+            'name'      => 'Dimora Le Torri',
+            'zipcode'   => "50122"
         ],
         [
-            'city' => 'Firenze',
-            'address' => 'Viale Francesco Redi, 63',
-            'name' => 'San Jacopino - Flo Apartments'
+            'city'      => 'Firenze',
+            'address'   => 'Viale Francesco Redi, 63',
+            'name'      => 'San Jacopino - Flo Apartments',
+            'zipcode'   => "50144"
         ],
         [
-            'city' => 'Firenze',
-            'address' => 'Via Palazzo dei Diavoli, 18',
-            'name' => 'Florence Comfort B&B'
+            'city'      => 'Firenze',
+            'address'   => 'Via Palazzo dei Diavoli, 18',
+            'name'      => 'Florence Comfort B&B',
+            'zipcode'   => "50142"
         ],
         [
-            'city' => 'Firenze',
-            'address' => 'Via dei Banchi, 6',
-            'name' => 'Residenza Mondragone'
+            'city'      => 'Firenze',
+            'address'   => 'Via dei Banchi, 6',
+            'name'      => 'Residenza Mondragone',
+            'zipcode'   => "50123"
         ],
         [
-            'city' => 'Roma',
-            'address' => 'Via del Corso, 292',
-            'name' => 'Sonder Piazza Venezia'
+            'city'      => 'Roma',
+            'address'   => 'Via del Corso, 292',
+            'name'      => 'Sonder Piazza Venezia',
+            'zipcode'   => "00186"
         ],
         [
-            'city' => 'Roma',
-            'address' => 'Via Tuscolana, 4, int. 25, scala B',
-            'name' => 'Re di Roma - Charme Apartment'
+            'city'      => 'Roma',
+            'address'   => 'Via Tuscolana, 4, int. 25, scala B',
+            'name'      => 'Re di Roma - Charme Apartment',
+            'zipcode'   => "00173"
         ],
         [
-            'city' => 'Roma',
-            'address' => 'Via del Governo Vecchio, 118',
-            'name' => 'Passepartout'
+            'city'      => 'Roma',
+            'address'   => 'Via del Governo Vecchio, 118',
+            'name'      => 'Passepartout',
+            'zipcode'   => "00186"
         ],
         [
-            'city' => 'Roma',
-            'address' => 'Via degli Avignonesi, 20',
-            'name' => 'Daphne Luxury Suites'
+            'city'      => 'Roma',
+            'address'   => 'Via degli Avignonesi, 20',
+            'name'      => 'Daphne Luxury Suites',
+            'zipcode'   => "00187"
         ],
         [
-            'city' => 'Milano',
-            'address' => 'Corso Italia, 11 A',
-            'name' => 'Sonder Missori'
+            'city'      => 'Milano',
+            'address'   => 'Corso Italia, 11 A',
+            'name'      => 'Sonder Missori',
+            'zipcode'   => "20122"
         ],
         [
-            'city' => 'Milano',
-            'address' => 'Via Feltre, 71',
-            'name' => 'Green Guest House'
+            'city'      => 'Milano',
+            'address'   => 'Via Feltre, 71',
+            'name'      => 'Green Guest House',
+            'zipcode'   => "20132"
         ],
         [
-            'city' => 'Milano',
-            'address' => 'Via Pergolesi, 31',
-            'name' => 'Milan Apartment Rental'
+            'city'      => 'Milano',
+            'address'   => 'Via Pergolesi, 31',
+            'name'      => 'Milan Apartment Rental',
+            'zipcode'   => "20124"
         ],
         [
-            'city' => 'Milano',
-            'address' => 'Via Bernardino Zenale, 9',
-            'name' => 'Magenta - Zenale Charme Loft'
+            'city'      => 'Milano',
+            'address'   => 'Via Bernardino Zenale, 9',
+            'name'      => 'Magenta - Zenale Charme Loft',
+            'zipcode'   => "20123"
         ],
         [
-            'city' => 'Venezia',
-            'address' => 'Calle Dei Boteri San Polo, 1699',
-            'name' => 'Ai Boteri'
+            'city'      => 'Venezia',
+            'address'   => 'Calle Dei Boteri San Polo, 1699',
+            'name'      => 'Ai Boteri',
+            'zipcode'   => "30125"
         ],
         [
-            'city' => 'Venezia',
-            'address' => 'Calle Larga Giacinto Gallina, 5400',
-            'name' => 'Ca Cappellis'
+            'city'      => 'Venezia',
+            'address'   => 'Calle Larga Giacinto Gallina, 5400',
+            'name'      => 'Ca Cappellis',
+            'zipcode'   => "30121"
         ],
         [
-            'city' => 'Venezia',
-            'address' => 'Cannaregio, 2893',
-            'name' => 'Locanda del Ghetto'
+            'city'      => 'Venezia',
+            'address'   => 'Cannaregio, 2893',
+            'name'      => 'Locanda del Ghetto',
+            'zipcode'   => "30121"
         ],
         [
-            'city' => 'Venezia',
-            'address' => 'Campo S. Maria Formosa, 5240',
-            'name' => 'Hotel Scandinavia - Relais'
+            'city'      => 'Venezia',
+            'address'   => 'Campo S. Maria Formosa, 5240',
+            'name'      => 'Hotel Scandinavia - Relais',
+            'zipcode'   => "30122"
         ],
         [
-            'city' => 'Bologna',
-            'address' => 'Via Claudio Treves, 13',
-            'name' => 'A Casa di Giancarla'
+            'city'      => 'Bologna',
+            'address'   => 'Via Claudio Treves, 13',
+            'name'      => 'A Casa di Giancarla',
+            'zipcode'   => "40135"
         ],
         [
-            'city' => 'Bologna',
-            'address' => 'Via Marsala, 39',
-            'name' => 'Lovely Flat, ground floor center of Bologna'
+            'city'      => 'Bologna',
+            'address'   => 'Via Marsala, 39',
+            'name'      => 'Lovely Flat, ground floor center of Bologna',
+            'zipcode'   => "40126"
         ],
         [
-            'city' => 'Bologna',
-            'address' => 'Via Guglielmo Marconi, 2',
-            'name' => 'La Dotta Apartments'
+            'city'      => 'Bologna',
+            'address'   => 'Via Guglielmo Marconi, 2',
+            'name'      => 'La Dotta Apartments',
+            'zipcode'   => "40122"
         ],
         [
-            'city' => 'Bologna',
-            'address' => 'Viale Angelo Masini, 4',
-            'name' => 'Smy Bologna Centrale'
+            'city'      => 'Bologna',
+            'address'   => 'Viale Angelo Masini, 4',
+            'name'      => 'Smy Bologna Centrale',
+            'zipcode'   => "40126"
         ],
         [
-            'city' => 'Torino',
-            'address' => 'Via Saluzzo, 7',
-            'name' => 'Hotel Urbani'
+            'city'      => 'Torino',
+            'address'   => 'Via Saluzzo, 7',
+            'name'      => 'Hotel Urbani',
+            'zipcode'   => "10125"
         ],
         [
-            'city' => 'Torino',
-            'address' => 'Piazza Vittorio Veneto, 13',
-            'name' => 'Piazza Vittorio Suites'
+            'city'      => 'Torino',
+            'address'   => 'Piazza Vittorio Veneto, 13',
+            'name'      => 'Piazza Vittorio Suites',
+            'zipcode'   => "10124"
         ],
         [
-            'city' => 'Torino',
-            'address' => 'Via Santa Teresa, 18',
-            'name' => 'Top Living Apartments - Torino Centro'
+            'city'      => 'Torino',
+            'address'   => 'Via Santa Teresa, 18',
+            'name'      => 'Top Living Apartments - Torino Centro',
+            'zipcode'   => "10121"
         ],
         [
-            'city' => 'Torino',
-            'address' => 'Via A. Bonafous, 5',
-            'name' => 'Hotel Alpi Resort'
+            'city'      => 'Torino',
+            'address'   => 'Via A. Bonafous, 5',
+            'name'      => 'Hotel Alpi Resort',
+            'zipcode'   => "10123"
         ],
         [
-            'city' => 'Napoli',
-            'address' => 'Via Ferdinando Palasciano, 17',
-            'name' => 'Due Cerase a Chiaia'
+            'city'      => 'Napoli',
+            'address'   => 'Via Ferdinando Palasciano, 17',
+            'name'      => 'Due Cerase a Chiaia',
+            'zipcode'   => "80122"
         ],
         [
-            'city' => 'Napoli',
-            'address' => 'Discesa Gaiola, 73',
-            'name' => 'Residenza Le Rose Villa d Arte',
+            'city'      => 'Napoli',
+            'address'   => 'Discesa Gaiola, 73',
+            'name'      => 'Residenza Le Rose Villa d Arte',
+            'zipcode'   => "80123"
         ],
         [
-            'city' => 'Napoli',
-            'address' => 'Via Cesare Rosaroll',
-            'name' => 'Le Porte di Napoli Rooms'
+            'city'      => 'Napoli',
+            'address'   => 'Via Cesare Rosaroll',
+            'name'      => 'Le Porte di Napoli Rooms',
+            'zipcode'   => "80139"
         ],
         [
-            'city' => 'Napoli',
-            'address' => 'Via Genova, 63',
-            'name' => 'Atlantic Ostello B&B'
+            'city'      => 'Napoli',
+            'address'   => 'Via Genova, 63',
+            'name'      => 'Atlantic Ostello B&B',
+            'zipcode'   => "80143"
         ],
         [
-            'city' => 'Palermo',
-            'address' => 'Via Enrico Toti, 112',
-            'name' => 'Le stanze di Aurora'
+            'city'      => 'Palermo',
+            'address'   => 'Via Enrico Toti, 112',
+            'name'      => 'Le stanze di Aurora',
+            'zipcode'   => "90128"
         ],
         [
-            'city' => 'Palermo',
-            'address' => 'Via Divisi, 74',
-            'name' => 'Divisi Suites & Spa'
+            'city'      => 'Palermo',
+            'address'   => 'Via Divisi, 74',
+            'name'      => 'Divisi Suites & Spa',
+            'zipcode'   => "90133"
         ],
         [
-            'city' => 'Palermo',
-            'address' => 'Cortile Giorgio II Greco',
-            'name' => 'Cortile Del Greco Apartments'
+            'city'      => 'Palermo',
+            'address'   => 'Cortile Giorgio II Greco',
+            'name'      => 'Cortile Del Greco Apartments',
+            'zipcode'   => "90134"
         ],
         [
-            'city' => 'Palermo',
-            'address' => 'Via San Gregorio, 18',
-            'name' => 'Mosina Apartments'
+            'city'      => 'Palermo',
+            'address'   => 'Via San Gregorio, 18',
+            'name'      => 'Mosina Apartments',
+            'zipcode'   => "90134"
         ],
         [
-            'city' => 'Bari',
-            'address' => 'Via Nicolò Piccinni, 191',
-            'name' => 'Le Stanze di Sofia'
+            'city'      => 'Bari',
+            'address'   => 'Via Nicolò Piccinni, 191',
+            'name'      => 'Le Stanze di Sofia',
+            'zipcode'   => "70122"
         ],
         [
-            'city' => 'Bari',
-            'address' => 'Via Abate Giacinto Gimma, 315',
-            'name' => 'VGM - Apartments Gimma'
+            'city'      => 'Bari',
+            'address'   => 'Via Abate Giacinto Gimma, 315',
+            'name'      => 'VGM - Apartments Gimma',
+            'zipcode'   => "70121"
         ],
         [
-            'city' => 'Bari',
-            'address' => 'Strada Nuccia Serra, 29',
-            'name' => 'Palazzo San Rocco'
+            'city'      => 'Bari',
+            'address'   => 'Strada Nuccia Serra, 29',
+            'name'      => 'Palazzo San Rocco',
+            'zipcode'   => "70122"
         ],
         [
-            'city' => 'Bari',
-            'address' => 'Via Sagarriga Visconti, 196',
-            'name' => 'La Pietra di San Tommaso - Central Station'
+            'city'      => 'Bari',
+            'address'   => 'Via Sagarriga Visconti, 196',
+            'name'      => 'La Pietra di San Tommaso - Central Station',
+            'zipcode'   => "70122"
         ],
         [
-            'city' => 'Ravenna',
-            'address' => 'Via Maggiore, 189',
-            'name' => 'Le Camere di Olivia'
+            'city'      => 'Ravenna',
+            'address'   => 'Via Maggiore, 189',
+            'name'      => 'Le Camere di Olivia',
+            'zipcode'   => "48121"
         ],
         [
-            'city' => 'Ravenna',
-            'address' => 'Via Dismano Vecchio, 38',
-            'name' => 'La Casina dell Artista',
+            'city'      => 'Ravenna',
+            'address'   => 'Via Dismano Vecchio, 38',
+            'name'      => 'La Casina dell Artista',
+            'zipcode'   => "48121"
         ],
         [
-            'city' => 'Ravenna',
-            'address' => 'Via Cerchio, 74',
-            'name' => 'Atmosfera B&B'
+            'city'      => 'Ravenna',
+            'address'   => 'Via Cerchio, 74',
+            'name'      => 'Atmosfera B&B',
+            'zipcode'   => "48121"
         ],
         [
-            'city' => 'Ravenna',
-            'address' => 'Via di Roma, 140',
-            'name' => 'Palazzo Galletti Abbiosi'
+            'city'      => 'Ravenna',
+            'address'   => 'Via di Roma, 140',
+            'name'      => 'Palazzo Galletti Abbiosi',
+            'zipcode'   => "48121"
         ],
         [
-            'city' => 'San Teodoro',
-            'address' => 'Località Badualga',
-            'name' => 'Hotel San Teodoro'
+            'city'      => 'San Teodoro',
+            'address'   => 'Località Badualga',
+            'name'      => 'Hotel San Teodoro',
+            'zipcode'   => "07052"
         ],
         [
-            'city' => 'San Teodoro',
-            'address' => 'Via Li Menduli',
-            'name' => 'Residence Li Menduli'
+            'city'      => 'San Teodoro',
+            'address'   => 'Via Li Menduli',
+            'name'      => 'Residence Li Menduli',
+            'zipcode'   => "08020"
         ],
         [
-            'city' => 'San Teodoro',
-            'address' => 'Via dei Gerani',
-            'name' => 'Gerani Bilocale'
+            'city'      => 'San Teodoro',
+            'address'   => 'Via dei Gerani',
+            'name'      => 'Gerani Bilocale',
+            'zipcode'   => "07052"
         ],
         [
-            'city' => 'San Teodoro',
-            'address' => 'Via di Li Muntigghieddi, snc',
-            'name' => 'Li Muntigeddi'
+            'city'      => 'San Teodoro',
+            'address'   => 'Via di Li Muntigghieddi, snc',
+            'name'      => 'Li Muntigeddi',
+            'zipcode'   => "07052"
         ],
         [
-            'city' => 'Cortina d Ampezzo',
-            'address' => 'Via Val di Sotto, 12',
-            'name' => 'Hotel Corona'
+            'city'      => 'Cortina d Ampezzo',
+            'address'   => 'Via Val di Sotto, 12',
+            'name'      => 'Hotel Corona',
+            'zipcode'   => "32043"
         ],
         [
-            'city' => 'Cortina d Ampezzo',
-            'address' => 'Via Stazione, 2',
-            'name' => 'Royal Hotel Cortina'
+            'city'      => 'Cortina d Ampezzo',
+            'address'   => 'Via Stazione, 2',
+            'name'      => 'Royal Hotel Cortina',
+            'zipcode'   => "32043"
         ],
         [
-            'city' => 'Cortina d Ampezzo',
-            'address' => 'Acquabona di Sotto',
-            'name' => 'The Wooden House Cortina'
+            'city'      => 'Cortina d Ampezzo',
+            'address'   => 'Acquabona di Sotto',
+            'name'      => 'The Wooden House Cortina',
+            'zipcode'   => "32043"
         ],
         [
-            'city' => 'Cortina d Ampezzo',
-            'address' => 'Catarina-Lanz, 99',
-            'name' => 'Les Ciases Chalets Dolomites Apt 3'
+            'city'      => 'Cortina d Ampezzo',
+            'address'   => 'Catarina-Lanz, 99',
+            'name'      => 'Les Ciases Chalets Dolomites Apt 3',
+            'zipcode'   => "32043"
         ],
         [
-            'city' => 'Perugia',
-            'address' => 'Via Matteo Renato Imbriani, 39',
-            'name' => 'Villa Imbriani'
+            'city'      => 'Perugia',
+            'address'   => 'Via Matteo Renato Imbriani, 39',
+            'name'      => 'Villa Imbriani',
+            'zipcode'   => "06122"
         ],
         [
-            'city' => 'Perugia',
-            'address' => 'Via del Grillo',
-            'name' => 'La Corte del Grillo'
+            'city'      => 'Perugia',
+            'address'   => 'Via del Grillo',
+            'name'      => 'La Corte del Grillo',
+            'zipcode'   => "06121"
         ],
         [
-            'city' => 'Perugia',
-            'address' => 'Via dedl Discobolo, 42',
-            'name' => 'Hotel la Meridiana'
+            'city'      => 'Perugia',
+            'address'   => 'Via del Discobolo, 42',
+            'name'      => 'Hotel la Meridiana',
+            'zipcode'   => "06132"
         ],
         [
-            'city' => 'Perugia',
-            'address' => 'Via degli Orti',
-            'name' => 'B&B Uva e Fragola'
+            'city'      => 'Perugia',
+            'address'   => 'Via degli Orti',
+            'name'      => 'B&B Uva e Fragola',
+            'zipcode'   => "06123"
         ],
     ];
