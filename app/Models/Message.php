78ea708b6f41cdda9e2b49242624f29177952d0f
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-07-15 16:40:41 +0900

    Model Message + metodi relazionali

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected   $table      =   'Messages';
+    
+    protected   $fillable   =   [
+                                    'apartment_id',
+                                    'name',
+                                    'surname',
+                                    'email',
+                                    'email_body'
+                                ];
+
+    public  function apartment()
+    {
+        return $this->belogsTo(Apartment::class);
+    } 
 }
