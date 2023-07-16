92297944978862a179bae4ed8724579fefac4533
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-07-16 18:02:52 +0900

    Migration per Pictures creata

--- database/migrations/2023_07_16_084809_create_pictures_table.php
+++ database/migrations/2023_07_16_084809_create_pictures_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('pictures', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('apartment_id');
+            $table->foreign('apartment_id')->references('id')->on('apartments')->onDelete('cascade');
+            $table->text('picture_url');
             $table->timestamps();
         });
     }
