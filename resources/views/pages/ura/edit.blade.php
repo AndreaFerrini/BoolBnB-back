968b4a3dc81e959c654126b2455e5dd74b76bec7
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-07-17 11:45:08 +0200

    modifiche layouts edit

--- resources/views/pages/ura/edit.blade.php
+++ resources/views/pages/ura/edit.blade.php
@@ -40,48 +40,60 @@
           </div>
         </div>
 
-        <div class="mb-3 form-group">
-        </div>
+        <div>
+          <div class="row">
+            <div class="col-6">
+              <div class="form-group">
+                <label for="apartments-address" class="form-label mt-4">Indirizzo</label>
+                <input type="text" required max="255" id="apartments-address" class="form-control" placeholder="Inserisci l'indirizzo dell'appartamento" name="address" value="{{ old('address') ?? $apartment->getIndirizzo() }}">
+                @error('address')
+                <span style="color: red; text-transform: uppercase">{{ $message }}</span>
+                @enderror
+              </div>
+              
+            </div>
+            <div class="col-2">
+              <div class="form-group">
+                <label for="apartments-address_number" class="form-label mt-4">Numero civico</label>
+                <input type="text" required max="9999" min="0001" id="apartments-address_number" class="form-control" placeholder="5B" name="address_number" value="{{ old('address_number') ?? $apartment->getCivico() }}" pattern="[0-9a-zA-Z]+">
+                @error('address_number')
+                <span style="color: red; text-transform: uppercase">{{ $message }}</span>
+                @enderror
+              </div>
 
+            </div>
+            <div class="col-2">
+              <div class="form-group">
+                <label for="apartments-city" class="form-label mt-4">Città</label>
+                <select name="city" id="apartments-city" class="form-control" required>
+                  <option disabled selected>Scegli una città</option>
+                  @foreach ($cities as $city)
+                    <option {{ old('city',$apartment->city) == $city ? 'selected' : '' }}>{{ $city }}</option>
+                  @endforeach
+                </select>
+              </div>
 
-        <div class="form-group">
-          <label for="apartments-address" class="form-label mt-4">Indirizzo</label>
-          <input type="text" required max="255" id="apartments-address" class="form-control" placeholder="Inserisci l'indirizzo dell'appartamento" name="address" value="{{ old('address') ?? $apartment->getIndirizzo() }}">
-          @error('address')
-          <span style="color: red; text-transform: uppercase">{{ $message }}</span>
-          @enderror
-        </div>
-
-        <div class="form-group">
-          <label for="apartments-address_number" class="form-label mt-4">Numero civico</label>
-          <input type="text" required max="9999" min="0001" id="apartments-address_number" class="form-control" placeholder="5B" name="address_number" value="{{ old('address_number') ?? $apartment->getCivico() }}" pattern="[0-9a-zA-Z]+">
-          @error('address_number')
-          <span style="color: red; text-transform: uppercase">{{ $message }}</span>
-          @enderror
-        </div>
-
-        <div class="form-group">
-          <label for="apartments-city" class="form-label mt-4">Città</label>
-          <select name="city" id="apartments-city" class="form-control" required>
-            <option disabled selected>Scegli una città</option>
-            @foreach ($cities as $city)
-              <option {{ old('city',$apartment->city) == $city ? 'selected' : '' }}>{{ $city }}</option>
-            @endforeach
-          </select>
-        </div>
-
-        <div class="row">
-          <div class="col-md-4">
-            <div class="form-group mt-4">
-              <label for="apartments-postal_code" class="form-label">Codice postale</label>
-              <input type="text" required max="5" min="5" id="apartments-postal_code" class="form-control" placeholder="35010" name="postal_code" value="{{ old('address') ?? $apartment->getCap() }}" pattern="[0-9]+">
-              @error('postal_code')
-              <span style="color: red; text-transform: uppercase">{{ $message }}</span>
-              @enderror
+            </div>
+            <div class="col-2">
+              <div class="form-group mt-4">
+                <label for="apartments-postal_code" class="form-label">Codice postale</label>
+                <input type="text" required max="5" min="5" id="apartments-postal_code" class="form-control" placeholder="35010" name="postal_code" value="{{ old('address') ?? $apartment->getCap() }}" pattern="[0-9]+">
+                @error('postal_code')
+                <span style="color: red; text-transform: uppercase">{{ $message }}</span>
+                @enderror
+              </div>
+              
             </div>
           </div>
+        </div>
 
-          <div class="col-md-4">
+
+
+
+
+        <div class="row">
+
+          <div class="col-3">
             <div class="form-group mt-4">
               <label for="apartments-number_of_rooms" class="form-label">Numero stanze</label>
               <input type="number" required max="20" min="1" id="apartments-number_of_rooms" class="form-control" placeholder="3" name="number_of_rooms" value="{{ old('number_of_rooms') ?? $apartment->number_of_rooms }}" pattern="[0-9]+">
@@ -91,7 +103,7 @@
             </div>
           </div>
 
-          <div class="col-md-4">
+          <div class="col-3">
             <div class="form-group mt-4">
               <label for="apartments-number_of_bathrooms" class="form-label">Numero bagni</label>
               <input type="number" required max="20" min="1" id="apartments-number_of_bathrooms" class="form-control" placeholder="2" name="number_of_bathrooms" value="{{ old('number_of_bathrooms') ?? $apartment->number_of_bathrooms }}" pattern="[0-9]+">
@@ -100,10 +112,9 @@
               @enderror
             </div>
           </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-4">
+          
+          
+          <div class="col-3">
             <div class="form-group mt-4">
               <label for="apartments-square_meters" class="form-label">Metri quadrati</label>
               <input type="number" required max="1000" min="1" id="apartments-square_meters" class="form-control" placeholder="50" name="square_meters" value="{{ old('square_meters') ?? $apartment->square_meters }}" pattern="[0-9]+">
@@ -112,8 +123,8 @@
               @enderror
             </div>
           </div>
-
-          <div class="col-md-4">
+          
+          <div class="col-3">
             <div class="form-group mt-4">
               <label for="apartments-price" class="form-label">Prezzo</label>
               <input type="number" required max="99999" min="1" id="apartments-price" class="form-control" placeholder="499,99" name="price" value="{{ old('price') ?? $apartment->price }}" step="0.01" pattern="\d+(\.\d{1,2})?">
@@ -124,11 +135,13 @@
           </div>
         </div>
 
+
         <div class="form-group">
           <label class="form-label mt-4">Service:</label>
           <div class="form-check">
+            <div class="row">
             @foreach ($services as $service)
-              <div class="form-check">
+              <div class="form-check col-2">
                 @if ($errors->any())
                   <input class="form-check-input" type="checkbox" name="services[]" value="{{ $service->id }}" id="post-checkbox-{{ $service->id }}" {{ in_array($service->id, old('services', [])) ? 'checked' : '' }}>
                 @else
@@ -139,15 +152,16 @@
                 </label>
               </div>
             @endforeach
+            </div>
           </div>
         </div>
 
         <div class="form-group">
           <label class="form-label mt-4">Visibility:</label>
           <div>
-            <label for="apartments-visibility" class="form-label mt-4">Privato</label>
+            <label for="apartments-visibility" class="form-label mt-2">Privato</label>
             <input type="radio" name="visibility" value="0" checked="{{ old('visibility',$apartment->visibility) == 0 ? 'checked' : '' }}">
-            <label for="apartments-visibility" class="form-label mt-4">Visibile</label>
+            <label for="apartments-visibility" class="form-label mt-2">Visibile</label>
             <input type="radio" name="visibility" value="1" checked="{{ old('visibility',$apartment->visibility) == 1 ? 'checked' : '' }}">
           </div>
         </div>
