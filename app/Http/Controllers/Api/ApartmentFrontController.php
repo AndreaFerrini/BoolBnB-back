9438c3a2a94f0e3e1788982405fac32fb7998589
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-07-20 17:23:49 +0200

    dati sponsor con data scadenza <= ad oggi

--- app/Http/Controllers/Api/ApartmentFrontController.php
+++ app/Http/Controllers/Api/ApartmentFrontController.php
@@ -6,12 +6,20 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Apartment as Apartment;
 use App\Models\Sponsor as Sponsor;
+use Carbon\Carbon;
 
 class ApartmentFrontController extends Controller
 {
     public function index(Request $request)
     {
-        $apartments = Apartment::with(['pictures', 'services', 'sponsors', 'messages'])->get();
+        $today = Carbon::today();
+
+        // manda solo i dati che hanno o hanno avuto una sponsor e la data di scadenza della sponsor deve essere uguale o maggiore a oggi che ricaviamo con $today
+        $apartments = Apartment::with(['pictures', 'services', 'sponsors', 'messages'])
+            ->whereHas('sponsors', function ($query) use ($today) {
+                $query->where('expire_at', '>=', $today->toDateString());
+            })
+            ->get();
 
         if ($apartments->isNotEmpty())
         {
