103d58bba8c6ea2d2136e78539c100aa20b0d1b2
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-07-20 19:25:24 +0900

    Acquisizione indirizzo front-end ok

--- app/Http/Controllers/Api/FrontendUrlController.php
+++ app/Http/Controllers/Api/FrontendUrlController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class FrontendUrlController extends Controller
 {
-    //
+    public function save_data(Request $request)
+    {
+        if (session_status() !== PHP_SESSION_ACTIVE) 
+        {
+            session_start();
+        }
+        $_SESSION['front_url'] = $request->front_url;
+        return response()->json(['success' => true, 'value' => $request-> front_url]);
+    }
 }
