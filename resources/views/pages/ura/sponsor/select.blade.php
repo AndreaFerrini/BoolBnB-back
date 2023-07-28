01b6e2a2af10a8eb59ed4b67167712461ab505af
edoardope <pencoedoardo@gmail.com>
2023-07-28 22:39:04 +0200

    payment finished

--- resources/views/pages/ura/sponsor/select.blade.php
+++ resources/views/pages/ura/sponsor/select.blade.php
@@ -20,6 +20,7 @@
                 <h2 class="">BRONZE SPONSOR</h2>
                 <hr>
                 <p class="pt-3">Entra nella vetrina del successo con il nostro pacchetto Bronze Sponsor della durata di 24 ore! Ottieni visibilità esclusiva e raggiungi il tuo target attraverso la nostra piattaforma.</p>
+                <span>price: <strong>2.99</strong></span>
                 <form action="{{ route('checkout', $apartment_id) }}" method="post">
                   @csrf
                     <input type="hidden" name="input" value="2.99">
@@ -32,6 +33,7 @@
                <h2 class="">SILVER SPONSOR</h2>
                <hr>
                <p class="pt-3">Porta la tua inserzione al livello successivo con il nostro pacchetto Silver Sponsor di 72 ore! Approfitta di una maggiore esposizione e visibilità durante il lungo periodo dell'evento. Scegli il pacchetto Silver Sponsor e diventa un leader nel mercato.</p>
+               <span>price: <strong>5.99</strong></span>
                <form action="{{ route('checkout', $apartment_id) }}" method="post">
                   @csrf
                     <input type="hidden" name="input" value="5.99">
@@ -41,10 +43,10 @@
         </div>
         <div class="col-10 col-sm-3 col-xl-3 m-2">
             <div class="p-sponsor-card text-center p-4">
-               <h2 class="">GOLD</h2>
-               <h2>SONSOR</h2>
+               <h2 class="">GOLD SONSOR</h2>
                <hr>
                <p class="pt-3">Eccellenza e prestigio ti aspettano con il nostro pacchetto Gold Sponsor della durata di 144 ore! Con una presenza di lunga durata, il pacchetto Gold Sponsor ti permette di mettere in mostra il tuo immobile a un vasto pubblico. Prendi il posto che meriti come protagonista.</p>
+               <span>price: <strong>9.99</strong></span>
                <form action="{{ route('checkout', $apartment_id) }}" method="post">
                   @csrf
                     <input type="hidden" name="input" value="9.99">
