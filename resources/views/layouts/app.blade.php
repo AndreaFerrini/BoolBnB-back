f3892047e351a03698141af31606caaf41e56c84
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-07-31 16:08:35 +0200

    importato footer nel backend + estetica

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -190,6 +190,85 @@
         <main class="">
             @yield('content')
         </main>
+        <footer>
+            <div class="container">
+                <div class="row justify-content-between align-items-center">
+                    <div class="row col-12 col-md-9">
+                        <div class="col-6">
+                                <ul>
+                                    <li class="mt-1" id="paragrafi">Informazioni</li>
+                                </ul>
+                            <div class="row">
+                                <div class="col-md-6 col-12">
+                                    <ul class="flex-column align-items-center">
+                                        <li class="mt-1">
+                                            <a href="/ChiSiamo">Chi Siamo</a>
+                                        </li>
+                                        <li class="mt-1"></li>
+                                        <li class="mt-1">Lavora con noi</li>
+                                        <li class="mt-1">Marchio</li>
+                                    </ul>
+                                </div>
+                                <!-- <div class="col-md-6 col-12">
+                                    <ul class="flex-column align-items-center">
+                                        <li class="mt-1">Sede</li>
+                                        <li class="mt-1">Assistenza</li>
+                                    </ul>       
+                                </div> -->
+                            </div>
+                        </div>
+                        <div class="col-6">
+                                <ul>
+                                    <li class="mt-1" id="paragrafi">Norme</li>
+                                </ul>
+                            <div class="row">
+                                <div class="col-md-6 col-12">
+                                    <ul class="flex-column align-items-center">
+                                        <li class="mt-1"> Privacy</li>
+                                        <li class="mt-1">Termini</li>
+                                        <li class="mt-1">Sicurezza</li>
+                                    </ul>
+                                </div>
+                                <!-- <div class="col-md-6 col-12">
+                                    <ul class="flex-column align-items-center">
+                                        <li class="mt-1">Licenze</li>
+                                        <li class="mt-1">Cookie</li>
+                                        <li class="mt-1">Piani</li>
+                                    </ul>       
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row col-12 col-md-3 mx-auto">
+                      <div class="mt-2 d-none d-md-none d-sm-none d-lg-block d-xl-block d-xxl-block">
+                        <ul>
+                            <li class="mt-1 text-center" id="paragrafi">Partner</li>
+                            <li class="text-center">
+                                <img src="storage\app\public\TomTom-Logo.png" alt="" id="logo-partner1">
+                            </li>
+                            <li class="text-center">
+                                <img src="storage\app\public\braintree-logo-black.png" alt="" id="logo-partner2">
+                            </li>
+                        </ul>
+                      </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <hr>
+            </div>
+            <div class="container d-flex justify-content-between align-items-center">
+                <div>
+                    <img src="storage\app\public\BoolBnB.png" alt="">
+                    <span>&#169; BoolBnB 2023</span>
+                </div>
+                <div class="registrazione">
+                    <button class="btn btn-primary" id="footer-button">
+                        <a href="http://127.0.0.1:8000/register">Registrati</a>
+                    </button>
+                </div>
+            </div>
+        </footer>
     </div>
 </body>
 
